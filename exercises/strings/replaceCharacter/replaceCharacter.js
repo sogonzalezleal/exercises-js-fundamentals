3dbddce48e254b84cc48432df1c60c003440a8a0
Sofia Gonzalez Leal <sogonzalezleal@davidson.edu>
2020-06-16 16:09:48 -0500

    first draft of replaceCharacter

--- exercises/strings/replaceCharacter/replaceCharacter.js
+++ exercises/strings/replaceCharacter/replaceCharacter.js
@@ -1,26 +1,16 @@
-/**
- * Given an input string, a target character to find, and a replacement character,
- * returns a copy of the input string with the target character replaced by the
- * replacement character.
- *
- * @example
- * replaceCharacter('hello', 'l', '8'); // => 'he88o'
- *
- * @param {string} string - The string to replace a character in
- * @param {string} target - The target letter
- * @param {string} replaceWith - The replacement letter
- * @returns {string} A copy of the input string with target replaced by replaceWith
- */
+
+let string = 'Sofia Gonzalez Leal'
+let target = 'a'
+let replaceWith = '7'
 
 function replaceCharacter(string, target, replaceWith) {
-  // This is your job. :)
+  return newString = string.replace(target, replaceWith)
 }
 
-if (require.main === module) {
-  console.log('Running sanity checks for replaceCharacter:');
-
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
+if (require.main) {
+  let newString = string.replace(target, replaceWith)
+  console.log(newString);
 }
+
 
 module.exports = replaceCharacter;
